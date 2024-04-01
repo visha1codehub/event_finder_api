ea2f759c5fd4a21a43a07926d112a7482d679e45
Vishal Gupta <itsvishalhack@gmail.com>
2024-04-01 11:24:33 +0530

    Created Event model..

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin    # noqa
+"""
+Admin for core..
+"""
 
-# Register your models here.
+from django.contrib import admin
+
+from .models import Event
+
+
+admin.site.register(Event)
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,18 @@
-from django.db import models    # noqa
+"""
+Models for Event Mangement System.
+"""
 
-# Create your models here.
+from django.db import models
+
+
+class Event(models.Model):
+    """model for event."""
+    event_name = models.CharField(max_length=254)
+    city_name = models.CharField(max_length=254)
+    date = models.DateField()
+    time = models.TimeField()
+    latitude = models.DecimalField(max_digits=9, decimal_places=9)
+    longitude = models.DecimalField(max_digits=9, decimal_places=9)
+
+    def __str__(self):
+        return self.event_name
