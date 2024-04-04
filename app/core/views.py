6041018ea74c217515c21e9a095f0fde9a20b984
Vishal Gupta <itsvishalhack@gmail.com>
2024-04-04 12:23:23 +0530

    Implemented a threading method for event list api view using ThreadPoolExecuter.

--- app/core/views.py
+++ app/core/views.py
@@ -20,6 +20,8 @@ import asyncio
 from asgiref.sync import sync_to_async
 from adrf.views import APIView as aAPIView
 
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 
 WCODE = "KfQnTWHJbg1giyB_Q9Ih3Xu3L9QOBDTuU5zwqVikZepCAzFut3rqsg"
 DCODE = "IAKvV2EvJa6Z6dEIUqqd7yGAu7IZ8gaH-a0QO6btjRc1AzFu8Y3IcQ"
@@ -42,13 +44,13 @@ DCODE = "IAKvV2EvJa6Z6dEIUqqd7yGAu7IZ8gaH-a0QO6btjRc1AzFu8Y3IcQ"
     ]
 )
 class SyncEventListView(generics.ListAPIView):
-    """API view for event list."""
+    """Synchronous API view for event list."""
     serializer_class = EventSerializer
     queryset = Event
     pagination_class = CustomPagination
 
     def get(self, request):
-        """Get the list of events."""
+        """Get the list of events synchronously."""
         s = time.perf_counter()
         latitude = request.query_params.get('latitude', None)
         longitude = request.query_params.get('longitude', None)
@@ -122,7 +124,7 @@ class EventCreateView(generics.CreateAPIView):
 class AsyncEventListView(aAPIView):
     """Asynchronous API view for event list."""
     async def get(self, request):
-        """Get the list of events."""
+        """Get the list of events asynchronously."""
         s = time.perf_counter()
         latitude = request.query_params.get('latitude', None)
         longitude = request.query_params.get('longitude', None)
@@ -148,7 +150,7 @@ class AsyncEventListView(aAPIView):
 
         async def fetch_weather_distance(data):
             """
-            Fetch weather for each events from external API asynchronously.
+            Fetch weather and distance for each events from external API asynchronously.
             """
             async with httpx.AsyncClient() as client:
                 tasks = []
@@ -194,3 +196,98 @@ class AsyncEventListView(aAPIView):
             return srl.data
         except Exception:
             raise Exception
+
+
+@extend_schema(
+    parameters=[
+        OpenApiParameter(
+            name='latitude',
+            description="User's latitude.",
+            required=True,
+            type=OpenApiTypes.NUMBER,
+        ),
+        OpenApiParameter(
+            name='longitude',
+            description="User's longitude.",
+            required=True,
+            type=OpenApiTypes.NUMBER,
+        ),
+    ]
+)
+class ThreadEventListView(generics.ListAPIView):
+    """Threading API view for event list."""
+
+    serializer_class = EventSerializer
+    queryset = Event
+    pagination_class = CustomPagination
+
+    def get(self, request):
+        """Get the list of events using threads."""
+        s = time.perf_counter()
+        latitude = request.query_params.get('latitude', None)
+        longitude = request.query_params.get('longitude', None)
+
+        if not all([latitude, longitude]):
+            return Response({"error": "Latitude and Longitude parameters are required."}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            latitude = Decimal(latitude)
+            longitude = Decimal(longitude)
+        except Exception as ex:
+            print(ex)
+            return Response({"error": "Invalid paramters."}, status=status.HTTP_400_BAD_REQUEST)
+
+        current_date = datetime.now().date()
+        end_date = current_date + timedelta(days=14)
+        next_14_days_events = self.queryset.objects.filter(date__range=[current_date, end_date])
+        paginator = CustomPagination()
+        result_page = paginator.paginate_queryset(next_14_days_events, request)
+        serializers = self.get_serializer(result_page, many=True)
+        data = serializers.data
+
+        def get_all(url, d_url, event, client):
+            """Thread Method for Hit the urls."""
+            weather_res = client.get(url)
+            weather_res_data = weather_res.json()
+            event['weather'] = weather_res_data["weather"]
+            distance_res = client.get(d_url)
+            distance_res_data = distance_res.json()
+            event['distance_km'] = distance_res_data["distance"]
+            return event
+
+        def fetch_weather_distance(data):
+            """
+            Fetch weather and distance for each events from external API using threads.
+            """
+            with ThreadPoolExecutor(max_workers=5) as executer:
+                with httpx.Client() as client:
+                    futures = []
+                    for event in data:
+                        latitude1 = latitude
+                        longitude1 = longitude
+                        latitude2 = event['latitude']
+                        longitude2 = event['longitude']
+                        city = event['city_name']
+                        date = event['date']
+                        w_url = f"https://gg-backend-assignment.azurewebsites.net/api/Weather?code={WCODE}==&city={city}&date={date}"    # noqa
+                        d_url = f"https://gg-backend-assignment.azurewebsites.net/api/Distance?code={DCODE}==&latitude1={latitude1}&longitude1={longitude1}&latitude2={latitude2}&longitude2={longitude2}"   # noqa
+                        futures.append(executer.submit(get_all, w_url, d_url, event, client))
+                    events_data = []
+                    try:
+                        for future in as_completed(futures):
+                            events_data.append(future.result())
+                        return events_data
+                    except Exception as ex:
+                        print(ex)
+
+        try:
+            data = fetch_weather_distance(data)
+
+            for item in data:
+                del item['time']
+                del item['latitude']
+                del item['longitude']
+            print("Thread Time: ", time.perf_counter()-s)
+            return paginator.get_paginated_response(data)
+        except httpx.HTTPError as e:
+            return Response({"error": str(e)},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
